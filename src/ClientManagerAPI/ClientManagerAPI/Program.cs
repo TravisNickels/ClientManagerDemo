1aecc20489cf85549a6d652ec23905907918a8a8
Travis Nickels <nickels.travis@gmail.com>
2025-10-19 01:56:53 -0700

    Add swagger DI

--- src/ClientManagerAPI/ClientManagerAPI/Program.cs
+++ src/ClientManagerAPI/ClientManagerAPI/Program.cs
@@ -7,10 +7,11 @@ var DbConfig = DatabaseConfig.Load();
 DbConfig.TestConnection();
 
 // Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(DbConfig.ToConnectionString()));
 
 var app = builder.Build();
@@ -19,6 +20,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
