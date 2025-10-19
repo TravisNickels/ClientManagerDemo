9d5241e789142868a6991a6e10f3a0e41075b1fb
Travis Nickels <nickels.travis@gmail.com>
2025-10-19 01:58:05 -0700

    Update the db context with models

--- src/ClientManagerAPI/ClientManagerAPI/Data/AppDbContext.cs
+++ src/ClientManagerAPI/ClientManagerAPI/Data/AppDbContext.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ClientManagerAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClientManagerAPI.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
+    public DbSet<Client> Clients { get; set; } = null!;
+    public DbSet<Phone> Phones { get; set; } = null!;
 }
 
