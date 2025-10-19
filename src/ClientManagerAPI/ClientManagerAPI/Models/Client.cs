68ae657d7c6415454127e7d9c96f7c730e416f33
Travis Nickels <nickels.travis@gmail.com>
2025-10-19 01:53:25 -0700

    Add client and phone database models

---
+++ src/ClientManagerAPI/ClientManagerAPI/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientManagerAPI.Models;
+
+public class Client
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [StringLength(100)]
+    public string? FirstName { get; set; }
+    [Required]
+    [StringLength(100)]
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+
+    public List<Phone> Phones { get; set; } = [];
+}
+
---
+++ src/ClientManagerAPI/ClientManagerAPI/Models/Phone.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClientManagerAPI.Models;
+
+public class Phone
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [ForeignKey("Client")]
+    public int ClientId { get; set; }
+    [Required]
+    public string Number { get; set; } = string.Empty;
+    [Required]
+    public string Type { get; set; } = string.Empty;
+
+    public Client Client { get; set; } = null!;
+}
+
