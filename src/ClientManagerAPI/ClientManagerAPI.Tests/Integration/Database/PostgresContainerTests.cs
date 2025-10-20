97c3bf32065a1c1f07ba705970673b01b15db5e7
Travis Nickels <nickels.travis@gmail.com>
2025-10-19 18:39:05 -0700

    Create postgres integration test for AppDbContext

---
+++ src/ClientManagerAPI/ClientManagerAPI.Tests/Integration/Database/PostgresContainerTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Testcontainers.PostgreSql;
+using ClientManagerAPI.Data;
+using FluentAssertions;
+
+namespace ClientManagerAPI.Tests.Integration.Database;
+
+[TestFixture]
+internal class PostgresContainerTests
+{
+    PostgreSqlContainer _container = null!;
+    protected AppDbContext _dbContext = null!;
+
+    [OneTimeSetUp]
+    public async Task Setup()
+    {
+        _container = new PostgreSqlBuilder()
+            .WithDatabase("clientManagerTestDb")
+            .WithUsername("postgres")
+            .WithPassword("postgres")
+            .Build();
+
+        await _container.StartAsync();
+        
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseNpgsql(_container.GetConnectionString())
+            .Options;
+        
+        _dbContext = new AppDbContext(options);
+        
+        // Ensure database is created - bypasses migrations for testing
+        await _dbContext.Database.EnsureCreatedAsync();
+    }
+
+    [OneTimeTearDown]
+    public async Task DisposeAsync()
+    {
+        if(_container is not null)
+            await _container.DisposeAsync();
+        if(_dbContext is not null)
+            await _dbContext.DisposeAsync();
+    }
+
+    [Test]
+    public async Task TestDatabaseConnection()
+    {
+        // Act
+        var canConnect = await _dbContext.Database.CanConnectAsync();
+        // Assert
+        canConnect.Should().BeTrue();
+    }
+}
+
