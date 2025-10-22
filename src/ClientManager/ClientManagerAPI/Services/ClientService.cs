f4d3c5ca4ac40408bc20b527d2922fb2c1dee956
Travis Nickels <nickels.travis@gmail.com>
2025-10-21 20:45:30 -0700

    Add Client service to generate GUID ID for new client

---
+++ src/ClientManager/ClientManagerAPI/Services/ClientService.cs
@@ -0,0 +1,18 @@
+﻿using ClientManagerAPI.Models;
+using ClientManagerAPI.Repositories;
+
+namespace ClientManagerAPI.Services;
+
+public class ClientService(IClientRepository clientRepository)
+{
+    IClientRepository _clientRepository = clientRepository;
+
+    public async Task<Client> CreateClientAsync(Client client)
+    {
+        if (client.Id == Guid.Empty)
+            client.Id = Guid.NewGuid();
+
+        return await _clientRepository.AddAsync(client);
+    }
+}
+
