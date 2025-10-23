060e49cda51b0ebfebda97c2d5dd3a0d4813282a
Travis Nickels <nickels.travis@gmail.com>
2025-10-23 05:14:05 -0700

    Update the worker project configuration

---
+++ src/ClientManager/ClientManager.Worker/Config/ConfigHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace ClientManager.Worker.Config;
+
+public class ConfigHelper
+{
+    public static string? FindEnvFile(string startDirectory)
+    {
+        var currentDir = new DirectoryInfo(startDirectory);
+        while (currentDir != null)
+        {
+            var envFilePath = Path.Combine(currentDir.FullName, ".env");
+            if (File.Exists(envFilePath))
+            {
+                return envFilePath;
+            }
+            currentDir = currentDir.Parent;
+        }
+        return null;
+    }
+}
+
