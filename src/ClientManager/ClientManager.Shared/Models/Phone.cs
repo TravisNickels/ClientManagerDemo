f7c2b01db0f08ba9d93a6fbd646dbdccc72143bd
Travis Nickels <nickels.travis@gmail.com>
2025-10-23 04:13:17 -0700

    Add shared project for models

---
+++ src/ClientManager/ClientManager.Shared/Models/Phone.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClientManager.Shared.Models;
+
+public class Phone
+{
+    [Key]
+    public Guid Id { get; set; }
+    [Required]
+    [ForeignKey("Client")]
+    public Guid ClientId { get; set; }
+    [Required]
+    public string Number { get; set; } = string.Empty;
+    [Required]
+    public string Type { get; set; } = string.Empty;
+
+    public Client Client { get; set; } = null!;
+}
+
