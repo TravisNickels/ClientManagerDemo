652c79649d8e7b6ba0b809228afb1114995ff6bf
Travis Nickels <nickels.travis@gmail.com>
2025-10-25 13:38:47 -0700

    Rename interface and remove send function for factory

---
+++ src/ClientManager/ClientManager.Shared/Messaging/IMessageBrokerFactory.cs
@@ -0,0 +1,10 @@
+﻿using RabbitMQ.Client;
+
+namespace ClientManager.Shared.Messaging;
+
+public interface IMessageBrokerFactory : IAsyncDisposable
+{
+    ValueTask<IConnection> GetConnectionAsync();
+    ValueTask<IChannel> GetChannelAsync(string channelName, CreateChannelOptions? options = null);
+    ConnectionFactory GetConnectionFactory();
+}
