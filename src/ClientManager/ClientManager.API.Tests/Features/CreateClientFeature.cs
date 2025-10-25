557252be13cb05c3f327139aca2eb9145daa5a48
Travis Nickels <nickels.travis@gmail.com>
2025-10-24 18:16:44 -0700

    Update API tests with rabbitmq container setup

--- src/ClientManager/ClientManager.API.Tests/Features/CreateClientFeature.cs
+++ src/ClientManager/ClientManager.API.Tests/Features/CreateClientFeature.cs
@@ -1,7 +1,7 @@
-﻿using ClientManager.Shared.Models;
-using Testcontainers.PostgreSql;
+﻿using ClientManager.API.Services;
+using ClientManager.Shared.Messaging;
+using ClientManager.Shared.Models;
 using FluentAssertions;
-using Moq;
 using Testcontainers.RabbitMq;
 
 namespace ClientManager.API.Tests.Features;
@@ -9,9 +9,34 @@ namespace ClientManager.API.Tests.Features;
 [TestFixture]
 internal class CreateClientFeature
 {
-    PostgreSqlContainer _postgresContainer = null!;
     RabbitMqContainer _rabbitMqConatiner = null!;
- 
+    MessageBrokerFactory _messageBrokerFactory = null!;
+
+    [OneTimeSetUp]
+    public async Task CreateRabbitMqContainer()
+    {
+        _rabbitMqConatiner = new RabbitMqBuilder()
+            .WithUsername("guest")
+            .WithPassword("guest")
+            .WithExposedPort("5672")
+            .WithCleanUp(true)
+            .Build();
+        await _rabbitMqConatiner.StartAsync();
+
+        var connectionString = _rabbitMqConatiner.GetConnectionString();
+        var hostname = _rabbitMqConatiner.Hostname;
+        var port = _rabbitMqConatiner.GetMappedPublicPort(5672);
+
+        _messageBrokerFactory = new MessageBrokerFactory(connectionString, port);
+    }
+
+    [OneTimeTearDown]
+    public async Task DisposeAsync()
+    {
+        if (_rabbitMqConatiner is not null)
+            await _rabbitMqConatiner.DisposeAsync();
+    }
+
     [Test]
     public async Task When_Creating_A_Valid_Client_The_Service_Should_Enqueue_It_For_Saving()
     {
@@ -24,7 +49,11 @@ internal class CreateClientFeature
             Email = "Anakin.Skywalker@gmail.com"
         };
 
+        // When sending the create client message
+        var clientService = new ClientService(_messageBrokerFactory);
+        var result = await clientService.SendCreateClientMessage(newClient);
 
+        // Then the client should be enqueued successfully
+        result.Should().BeTrue();
     }
 }
-
