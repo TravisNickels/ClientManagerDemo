b4711491b7e1c31697232d4935ae8cd9b7c5c28b
Travis Nickels <nickels.travis@gmail.com>
2025-10-26 16:32:56 -0700

    Add mapper for client entity

---
+++ src/ClientManager/ClientManager.API/Mappers/ClientMapper.cs
@@ -0,0 +1,24 @@
+﻿using ClientManager.Shared.DTOs.Requests;
+using ClientManager.Shared.DTOs.Responses;
+using ClientManager.Shared.Models;
+
+namespace ClientManager.API.Mappers;
+
+public static class ClientMapper
+{
+    public static ClientResponse ToResponse(Client client)
+    {
+        return new ClientResponse(client.Id, client.FirstName!, client.LastName!, client.Email!);
+    }
+
+    public static Client ToEntity(CreateClientRequest request)
+    {
+        return new Client
+        {
+            Id = request.Id ?? Guid.Empty,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+        };
+    }
+}
