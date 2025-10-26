256c243fe03db7b0f3da97c4d1a565ab66f820f8
Travis Nickels <nickels.travis@gmail.com>
2025-10-25 18:52:07 -0700

    Move some tests to unit tests

---
+++ src/ClientManager/ClientManager.API.Tests/Unit/ClientServiceTests.cs
@@ -0,0 +1,137 @@
+﻿using System.Text.Json;
+using ClientManager.API.Services;
+using ClientManager.Shared.Configuration;
+using ClientManager.Shared.Messaging;
+using ClientManager.Shared.Models;
+using FluentAssertions;
+using Moq;
+
+namespace ClientManager.API.Tests.Unit;
+
+[TestFixture]
+internal class ClientServiceTests
+{
+    Client client = null!;
+    Mock<IQueuePublisher> mockPublisher = null!;
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        mockPublisher = new Mock<IQueuePublisher>();
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        client = new Client
+        {
+            Id = Guid.Empty,
+            FirstName = "Luke",
+            LastName = "Skywalker",
+            Email = "Luke.Skywalker@gmail.com"
+        };
+    }
+
+    [Test]
+    public async Task When_Creating_Client_With_No_First_Name_The_Service_Should_Throw()
+    {
+        // Given an invalid client (missing first name)
+        var invalidClient = client;
+        invalidClient.FirstName = "";
+
+        var clientService = new ClientService(mockPublisher.Object);
+
+        // Then it should throw a domain validation exception
+        await FluentActions.Invoking(() => clientService.SendCreateClientMessage(invalidClient)).Should().ThrowAsync<ArgumentException>();
+    }
+
+    [Test]
+    public async Task When_Creating_Client_With_No_Last_Name_The_Service_Should_Throw()
+    {
+        // Given an invalid client (missing last name)
+        var invalidClient = client;
+        invalidClient.LastName = "";
+
+        var clientService = new ClientService(mockPublisher.Object);
+
+        // Then it should throw a domain validation exception
+        await FluentActions.Invoking(() => clientService.SendCreateClientMessage(invalidClient)).Should().ThrowAsync<ArgumentException>();
+    }
+
+    [Test]
+    public async Task When_Creating_Client_With_No_Email_The_Service_Should_Throw()
+    {
+        // Given an invalid client (missing email)
+        var invalidClient = client;
+        invalidClient.Email = "";
+
+        var clientService = new ClientService(mockPublisher.Object);
+
+        // Then it should throw a domain validation exception
+        await FluentActions.Invoking(() => clientService.SendCreateClientMessage(invalidClient)).Should().ThrowAsync<ArgumentException>();
+    }
+
+    [Test]
+    public async Task When_Creating_Client_With_An_Invalid_Email_The_Service_Should_Throw()
+    {
+        // Given an invalid client (invalid email)
+        var invalidClient = client;
+        invalidClient.Email = "invalid-email-address";
+
+        var clientService = new ClientService(mockPublisher.Object);
+
+        // Then it should throw a domain validation exception
+        await FluentActions
+            .Invoking(() => clientService.SendCreateClientMessage(invalidClient))
+            .Should()
+            .ThrowAsync<ArgumentException>()
+            .Where(ex => ex.Message.Contains("must have a valid email address"));
+    }
+
+    [Test]
+    public async Task When_QueuePublisher_Fails_The_Service_Should_Throw()
+    {
+        // Given a mock queue publisher that throws an exception
+        var mockPublisher = new Mock<IQueuePublisher>();
+        mockPublisher
+            .Setup(p => p.PublishAsync(It.IsAny<string>(), It.IsAny<ReadOnlyMemory<byte>>(), It.IsAny<string>(), It.IsAny<string>()))
+            .ThrowsAsync(new Exception("Broker down"));
+
+        // When sending the create client message
+        var clientService = new ClientService(mockPublisher.Object);
+        var newClient = client;
+
+        // Then it should throw an InvalidOperationException with inner exception message
+        var ex = await FluentActions
+            .Invoking(() => clientService.SendCreateClientMessage(newClient))
+            .Should()
+            .ThrowAsync<InvalidOperationException>()
+            .Where(ex => ex.InnerException!.Message.Contains("Broker down"));
+    }
+
+    [Test]
+    public async Task When_Creating_Valid_Client_The_Service_Should_Serialize_Payload_Correctly()
+    {
+        // Given a mock queue publisher that captures the published body
+        ReadOnlyMemory<byte>? capturedBody = null;
+
+        var mockPublisher = new Mock<IQueuePublisher>();
+        mockPublisher
+            .Setup(p => p.PublishAsync(It.IsAny<string>(), It.IsAny<ReadOnlyMemory<byte>>(), It.IsAny<string>(), It.IsAny<string>()))
+            .Callback<string, ReadOnlyMemory<byte>, string, string>((queue, body, exchange, routingKey) => capturedBody = body)
+            .Returns(Task.CompletedTask);
+
+        var clientService = new ClientService(mockPublisher.Object);
+
+        var newClient = client;
+
+        // When sending the create client message
+        await clientService.SendCreateClientMessage(newClient);
+
+        // Then the captured body should deserialize to the expected client
+        var deserialized = JsonSerializer.Deserialize<Client>(capturedBody!.Value.Span);
+        deserialized!.FirstName.Should().Be("Luke");
+        deserialized.LastName.Should().Be("Skywalker");
+        deserialized.Email.Should().Be("Luke.Skywalker@gmail.com");
+    }
+}
