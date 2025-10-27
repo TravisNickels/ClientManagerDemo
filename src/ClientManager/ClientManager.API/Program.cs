aebf9e86b3ef7c4fd5223d0558765b1b178ece09
Travis Nickels <nickels.travis@gmail.com>
2025-10-27 15:03:59 -0700

    Update API host configuration to add .env to environment variables

--- src/ClientManager/ClientManager.API/Program.cs
+++ src/ClientManager/ClientManager.API/Program.cs
@@ -1,8 +1,14 @@
 using ClientManager.API.Services;
 using ClientManager.Shared.Configuration;
 using ClientManager.Shared.Messaging;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Load .env file if it exists
+ConfigurationHelper.LoadDotEnvFile();
+builder.Configuration.AddEnvironmentVariables();
+builder.Services.Configure<RabbitMQConnectionConfiguration>(builder.Configuration.GetSection("RABBITMQ"));
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -12,7 +18,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IMessageBrokerFactory>(bf =>
 {
-    var connectionConfiguration = RabbitMQConnectionConfiguration.Load();
+    var connectionConfiguration = bf.GetRequiredService<IOptions<RabbitMQConnectionConfiguration>>().Value;
     return new MessageBrokerFactory(connectionConfiguration);
 });
 builder.Services.AddScoped<IQueuePublisher, QueuePublisher>();
