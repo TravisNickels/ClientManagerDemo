f526720d33c382309b567b1d2b7d52a6353fefc1
Travis Nickels <nickels.travis@gmail.com>
2025-10-27 14:58:05 -0700

    Update configuration helper to load the env file

--- src/ClientManager/ClientManager.Shared/Configuration/ConfigurationHelper.cs
+++ src/ClientManager/ClientManager.Shared/Configuration/ConfigurationHelper.cs
@@ -1,7 +1,21 @@
-﻿namespace ClientManager.Shared.Configuration;
+﻿using dotenv.net;
+
+namespace ClientManager.Shared.Configuration;
 
 public class ConfigurationHelper
 {
+    public static void LoadDotEnvFile(string? basePath = null)
+    {
+        basePath ??= AppContext.BaseDirectory;
+        var envPath = FindEnvFile(basePath);
+        if (envPath is not null && File.Exists(envPath))
+            DotEnv.Load(new DotEnvOptions(envFilePaths: [envPath]));
+        else
+            Console.WriteLine(
+                $"Warning: .env file not found at {basePath} or in any parent folders. Using appsettings.json for environment variables or default configuration."
+            );
+    }
+
     public static string? FindEnvFile(string startDirectory)
     {
         var currentDir = new DirectoryInfo(startDirectory);
@@ -9,9 +23,8 @@ public class ConfigurationHelper
         {
             var envFilePath = Path.Combine(currentDir.FullName, ".env");
             if (File.Exists(envFilePath))
-            {
                 return envFilePath;
-            }
+
             currentDir = currentDir.Parent;
         }
         return null;
