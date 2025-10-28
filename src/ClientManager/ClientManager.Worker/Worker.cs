eb5347148f9ade6d99ebf7b3b2f5790c8ace09ab
Travis Nickels <nickels.travis@gmail.com>
2025-10-28 14:09:17 -0700

    Update worker to call consumer startAsync

--- src/ClientManager/ClientManager.Worker/Worker.cs
+++ src/ClientManager/ClientManager.Worker/Worker.cs
@@ -1,19 +1,26 @@
+using ClientManager.Shared.Messaging;
+using ClientManager.Worker.Repositories;
+
 namespace ClientManager.Worker;
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
+public class Worker(ILogger<Worker> logger, IServiceScopeFactory serviceScopeFactory) : BackgroundService
 {
     readonly ILogger<Worker> _logger = logger;
+    readonly IServiceScopeFactory _serviceScopeFactory = serviceScopeFactory;
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        await using var scope = _serviceScopeFactory.CreateAsyncScope();
+        var consumer = scope.ServiceProvider.GetRequiredService<IMessageConsumer>();
+
+        await consumer.StartAsync(cancellationToken);
+        while (!cancellationToken.IsCancellationRequested)
         {
             if (_logger.IsEnabled(LogLevel.Information))
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
-            await Task.Delay(1000, stoppingToken);
+            await Task.Delay(1000, cancellationToken);
         }
     }
 }
-
