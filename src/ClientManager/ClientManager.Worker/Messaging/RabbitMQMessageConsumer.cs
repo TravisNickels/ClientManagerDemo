8f529793dc2b16a8b03bbfdf7972cebe2caf42b1
Travis Nickels <nickels.travis@gmail.com>
2025-10-28 14:07:42 -0700

    Add a rabbitMQ message consumer with message discovery

---
+++ src/ClientManager/ClientManager.Worker/Messaging/RabbitMQMessageConsumer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+using RabbitMQ.Client;
+
+namespace ClientManager.Shared.Messaging;
+
+public class RabbitMQMessageConsumer(IServiceScopeFactory serviceScopeFactory, IMessageBrokerFactory messageBrokerFactory, ILogger<RabbitMQMessageConsumer> logger)
+    : IMessageConsumer
+{
+    readonly IServiceScopeFactory _scopeFactory = serviceScopeFactory;
+    readonly IMessageBrokerFactory _messageBrokerFactory = messageBrokerFactory;
+    readonly ILogger<RabbitMQMessageConsumer> _logger = logger;
+    readonly ConcurrentDictionary<string, IChannel> _channels = new();
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        var messageTypes = DiscoverMessageTypes();
+
+        return Task.CompletedTask;
+    }
+
+    static IEnumerable<Type> DiscoverMessageTypes() =>
+        AppDomain
+            .CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => typeof(IMessage).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract)
+            .ToList();
+}
