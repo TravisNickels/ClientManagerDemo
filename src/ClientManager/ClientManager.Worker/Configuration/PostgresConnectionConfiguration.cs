485b20f00924f215a358c687ceb00f2c760c9b43
Travis Nickels <nickels.travis@gmail.com>
2025-10-28 13:14:40 -0700

    Refactor and rename postgres connection configuration

---
+++ src/ClientManager/ClientManager.Worker/Configuration/PostgresConnectionConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using ClientManager.Shared.Configuration;
+using dotenv.net;
+using Npgsql;
+
+namespace ClientManager.Worker.Configuration;
+
+public class PostgresConnectionConfiguration()
+{
+    public string Host { get; init; } = "localhost";
+    public int Port { get; init; } = 5432;
+    public string Database { get; init; } = "clientManagerDB";
+    public string Username { get; init; } = "postgres";
+    public string Password { get; init; } = "postgres";
+    public string Schema { get; init; } = "public";
+
+    public string ToConnectionString() => ToConnectionStringBuilder().ConnectionString;
+
+    public NpgsqlConnectionStringBuilder ToConnectionStringBuilder() =>
+        new()
+        {
+            Host = Host,
+            Port = Port,
+            Database = Database,
+            Username = Username,
+            Password = Password,
+            SearchPath = Schema
+        };
+
+    public void TestConnection()
+    {
+        try
+        {
+            using var connection = new NpgsqlConnection(ToConnectionString());
+            connection.Open();
+            Console.WriteLine("PostgreSQL connection successful!");
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"PostgreSQL connection failed: {ex.Message}");
+            throw new InvalidOperationException(ex.Message);
+        }
+    }
+}
