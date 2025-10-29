fd5a335841a88bf72b7a8271a73017b6b01316d7
Travis Nickels <nickels.travis@gmail.com>
2025-10-29 00:39:42 -0700

    Move AppDbContext to shared project

--- src/ClientManager/ClientManager.Shared/Data/AppDbContext.cs
+++ src/ClientManager/ClientManager.Shared/Data/AppDbContext.cs
@@ -1,11 +1,10 @@
 ﻿using ClientManager.Shared.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace ClientManager.Worker.Data;
+namespace ClientManager.Shared.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Client> Clients { get; set; } = null!;
     public DbSet<Phone> Phones { get; set; } = null!;
 }
-
