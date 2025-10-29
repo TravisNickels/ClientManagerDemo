a42196ef61fa0931ad133b69ff4f9922a024706b
Travis Nickels <nickels.travis@gmail.com>
2025-10-29 15:13:37 -0700

    Reinforce the readonly app db context for API

--- src/ClientManager/ClientManager.Shared/Data/ReadOnlyAppDbContext.cs
+++ src/ClientManager/ClientManager.Shared/Data/ReadOnlyAppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace ClientManager.Shared.Data;
 
@@ -9,5 +10,19 @@ public class ReadOnlyAppDbContext : AppDbContext
 
     public override int SaveChanges() => throw new InvalidOperationException("This context is read-only.");
 
+    public override int SaveChanges(bool acceptAllChangesOnSuccess) => throw new InvalidOperationException("This context is read-only.");
+
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) => throw new InvalidOperationException("This context is read-only.");
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default) =>
+        throw new InvalidOperationException("This context is read-only.");
+
+    public override EntityEntry<TEntity> Add<TEntity>(TEntity entity) => throw new InvalidOperationException("Cannot call 'Update' — this DbContext is read-only.");
+
+    public override ValueTask<EntityEntry<TEntity>> AddAsync<TEntity>(TEntity entity, CancellationToken cancellationToken = default) =>
+        throw new InvalidOperationException("Cannot call 'Update' — this DbContext is read-only.");
+
+    public override EntityEntry<TEntity> Update<TEntity>(TEntity entity) => throw new InvalidOperationException("Cannot call 'Update' — this DbContext is read-only.");
+
+    public override EntityEntry<TEntity> Remove<TEntity>(TEntity entity) => throw new InvalidOperationException("Cannot call 'Update' — this DbContext is read-only.");
 }
