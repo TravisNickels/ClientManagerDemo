c7a3d474dbf80c36e1496daaa9d8dcfb80c3472b
Travis Nickels <nickels.travis@gmail.com>
2025-10-29 00:40:53 -0700

    Add a readonly DB context for the API project

---
+++ src/ClientManager/ClientManager.Shared/Data/ReadOnlyAppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ClientManager.Shared.Data;
+
+public class ReadOnlyAppDbContext : AppDbContext
+{
+    public ReadOnlyAppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public override int SaveChanges() => throw new InvalidOperationException("This context is read-only.");
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) => throw new InvalidOperationException("This context is read-only.");
+}
