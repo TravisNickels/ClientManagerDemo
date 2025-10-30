f5bd60f7f8d657d7d6bfc21c9e6fb70b97b7026e
Travis Nickels <nickels.travis@gmail.com>
2025-10-29 17:14:11 -0700

    Refactor DI

--- src/ClientManager/ClientManager.API/Program.cs
+++ src/ClientManager/ClientManager.API/Program.cs
@@ -1,6 +1,8 @@
 using ClientManager.API.Services;
 using ClientManager.Shared.Configuration;
+using ClientManager.Shared.Data;
 using ClientManager.Shared.Messaging;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Load .env file if it exists
 ConfigurationHelper.LoadDotEnvFile();
 builder.Configuration.AddEnvironmentVariables();
+
+// Map configuration sections to strongly typed classes
+builder.Services.Configure<PostgresConnectionConfiguration>(builder.Configuration.GetSection("POSTGRES"));
 builder.Services.Configure<RabbitMQConnectionConfiguration>(builder.Configuration.GetSection("RABBITMQ"));
 
 // Add services to the container.
@@ -16,13 +21,22 @@ builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddScoped<IQueuePublisher, QueuePublisher>();
+builder.Services.AddScoped<IClientService, ClientService>();
 builder.Services.AddSingleton<IMessageBrokerFactory>(sp =>
 {
+    // Get RabbitMQ connection configuration from DI
     var connectionConfiguration = sp.GetRequiredService<IOptions<RabbitMQConnectionConfiguration>>().Value;
     return new MessageBrokerFactory(connectionConfiguration);
 });
-builder.Services.AddScoped<IQueuePublisher, QueuePublisher>();
-builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddDbContext<AppDbContext, ReadOnlyAppDbContext>(
+    (sp, options) =>
+    {
+        // Get Postgres connection configuration from DI
+        var postgresConfig = sp.GetRequiredService<IOptions<PostgresConnectionConfiguration>>().Value;
+        options.UseNpgsql(postgresConfig.ToConnectionString());
+    }
+);
 
 var app = builder.Build();
 
