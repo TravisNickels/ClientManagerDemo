c5a91fcfbb9099f3edb687c7b1f25341b1aabaec
Travis Nickels <nickels.travis@gmail.com>
2025-10-29 18:45:52 -0700

    Add create client command contract to decouple the db entity from the message contracts

---
+++ src/ClientManager/ClientManager.Shared/Contracts/Commands/CreateClient.cs
@@ -0,0 +1,11 @@
+﻿using ClientManager.Shared.Messaging;
+
+namespace ClientManager.Shared.Contracts.Commands;
+
+public class CreateClient : ICommand
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+}
