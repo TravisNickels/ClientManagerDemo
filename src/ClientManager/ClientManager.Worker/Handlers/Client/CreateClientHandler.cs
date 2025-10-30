91bacb48e434d5665029a93831f7407325a84571
Travis Nickels <nickels.travis@gmail.com>
2025-10-29 23:59:17 -0700

    Add CreateClient handler in worker

---
+++ src/ClientManager/ClientManager.Worker/Handlers/Client/CreateClientHandler.cs
@@ -0,0 +1,28 @@
+﻿using ClientManager.Shared.Contracts.Commands;
+using ClientManager.Shared.Messaging;
+using ClientManager.Shared.Models;
+using ClientManager.Worker.Repositories;
+
+namespace ClientManager.Worker.Handlers;
+
+public class CreateClientHandler(IClientRepository clientRepository, ILogger<CreateClientHandler> logger) : IMessageHandler<CreateClient>
+{
+    readonly IClientRepository _clientRepository = clientRepository;
+    readonly ILogger<CreateClientHandler> _logger = logger;
+
+    public async Task HandleAsync(CreateClient message, CancellationToken cancellationToken)
+    {
+        await _clientRepository.AddAsync(
+            new Client
+            {
+                Id = message.Id,
+                FirstName = message.FirstName,
+                LastName = message.LastName,
+                Email = message.Email,
+            }
+        );
+
+        _logger.LogInformation("ClientCreatedHandler processed {client}", message.FirstName);
+        //await notifier.EmitEventAsync("client.created", message);
+    }
+}
