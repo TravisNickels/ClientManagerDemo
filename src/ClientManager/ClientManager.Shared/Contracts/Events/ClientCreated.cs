93c6c21722c46b4206ca365e33d886f0794a74ce
Travis Nickels <nickels.travis@gmail.com>
2025-10-30 22:24:10 -0700

    Add a client created event

---
+++ src/ClientManager/ClientManager.Shared/Contracts/Events/ClientCreated.cs
@@ -0,0 +1,11 @@
+﻿using ClientManager.Shared.Messaging;
+
+namespace ClientManager.Shared.Contracts.Events;
+
+public class ClientCreated : IEvent
+{
+    public Guid ClientId { get; set; }
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+}
