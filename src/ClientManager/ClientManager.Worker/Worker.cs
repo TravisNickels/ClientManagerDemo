e6309f9f513063291c20277d96e1d61435c3007d
Travis Nickels <nickels.travis@gmail.com>
2025-10-30 22:26:02 -0700

    Refactor the worker loop

--- src/ClientManager/ClientManager.Worker/Worker.cs
+++ src/ClientManager/ClientManager.Worker/Worker.cs
@@ -9,17 +9,16 @@ public class Worker(ILogger<Worker> logger, IServiceScopeFactory serviceScopeFac
 
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+        }
+
         await using var scope = _serviceScopeFactory.CreateAsyncScope();
         var consumer = scope.ServiceProvider.GetRequiredService<IMessageConsumer>();
 
         await consumer.StartAsync(cancellationToken);
-        while (!cancellationToken.IsCancellationRequested)
-        {
-            if (_logger.IsEnabled(LogLevel.Information))
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
-            await Task.Delay(1000, cancellationToken);
-        }
+
+        await Task.Delay(Timeout.Infinite, cancellationToken);
     }
 }
