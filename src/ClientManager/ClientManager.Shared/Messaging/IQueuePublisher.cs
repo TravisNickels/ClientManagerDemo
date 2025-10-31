09bc03df39817c24a08c1827341c90b785553fcb
Travis Nickels <nickels.travis@gmail.com>
2025-10-30 22:24:35 -0700

    Make parameters optional

--- src/ClientManager/ClientManager.Shared/Messaging/IQueuePublisher.cs
+++ src/ClientManager/ClientManager.Shared/Messaging/IQueuePublisher.cs
@@ -2,5 +2,5 @@
 
 public interface IQueuePublisher
 {
-    Task PublishAsync(string queueName, ReadOnlyMemory<byte> body, string exchange, string routingKey);
+    Task PublishAsync(string queueName, ReadOnlyMemory<byte> body, string exchange = "", string routingKey = "");
 }
