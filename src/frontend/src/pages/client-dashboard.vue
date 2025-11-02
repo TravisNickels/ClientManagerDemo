2605779dfc0d6d4483f883e4f5df29f51f91ce59
Travis Nickels <nickels.travis@gmail.com>
2025-11-02 01:20:57 -0800

    Add dashboard with signalr store for testing

---
+++ src/frontend/src/pages/client-dashboard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useSignalRStore } from '@/stores/signalrStore'
+
+const signalR = useSignalRStore()
+
+const clients: string[] = signalR.messages
+</script>
+
+<template>
+  <div>Testsing</div>
+  <div v-for="client in clients" v-bind:key="client">
+    <div>{{ client }}</div>
+  </div>
+</template>
