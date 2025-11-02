c6b0c7410a8504f165e6158c7421001be00b5a74
Travis Nickels <nickels.travis@gmail.com>
2025-11-02 01:31:40 -0800

    Remove signalr from App.vue

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,20 +1,14 @@
-<script setup lang="ts">
-import { onMounted } from 'vue'
-import { useSignalRStore } from './stores/signalrStore'
-
-const signalR = useSignalRStore()
-
-onMounted(() => {
-  signalR.connect()
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/client-dashboard">Dashboard</router-link>
+    </div>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
