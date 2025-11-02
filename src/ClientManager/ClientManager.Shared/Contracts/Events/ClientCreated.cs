a1b4d8721d330ed0ec38f3b66e866f3b97c61538
Travis Nickels <nickels.travis@gmail.com>
2025-11-01 17:46:51 -0700

    Add response interface to client created event

--- src/ClientManager/ClientManager.Shared/Contracts/Events/ClientCreated.cs
+++ src/ClientManager/ClientManager.Shared/Contracts/Events/ClientCreated.cs
@@ -1,11 +1,14 @@
-﻿using ClientManager.Shared.Messaging;
+﻿using ClientManager.Shared.DTOs.Responses;
+using ClientManager.Shared.Messaging;
 
 namespace ClientManager.Shared.Contracts.Events;
 
-public class ClientCreated : IEvent
+public class ClientCreated : IEvent, IEventToResponse<ClientResponse>
 {
     public Guid ClientId { get; set; }
     public string FirstName { get; set; } = default!;
     public string LastName { get; set; } = default!;
     public string Email { get; set; } = default!;
+
+    public ClientResponse ToResponse() => new(ClientId, FirstName, LastName, Email);
 }
