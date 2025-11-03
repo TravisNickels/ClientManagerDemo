4fbdaef4233b9d8a54d4bf20365ed85f4f3cf89b
Travis Nickels <nickels.travis@gmail.com>
2025-11-02 18:31:37 -0800

    Move signalr connect to dashboard

--- src/frontend/src/pages/client-dashboard.vue
+++ src/frontend/src/pages/client-dashboard.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import { useSignalRStore } from '@/stores/signalrStore'
+import { onMounted } from 'vue'
 
 const signalR = useSignalRStore()
 
 const clients: string[] = signalR.messages
+
+onMounted(() => {
+  signalR.connect()
+})
 </script>
 
 <template>
