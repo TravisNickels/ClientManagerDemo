6e58afce47246464cd60ece9611731f36f299f6b
Travis Nickels <nickels.travis@gmail.com>
2025-11-10 11:07:03 -0800

    Create a fake logger for tests

---
+++ src/ClientManager/ClientManager.API.Tests/Fakes/FakeLogger.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ClientManager.API.Tests.Fakes;
+
+internal class FakeLogger<T> : ILogger<T>
+{
+    public List<(LogLevel Level, string Message)> Logs { get; } = [];
+
+    public IDisposable? BeginScope<TState>(TState state)
+        where TState : notnull => NullScope.Instance;
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) =>
+        Logs.Add((logLevel, formatter(state, exception)));
+
+    class NullScope : IDisposable
+    {
+        public static readonly NullScope Instance = new();
+
+        public void Dispose() { }
+    }
+}
