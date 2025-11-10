2bca77ed03c713941f0302161e44a05cf9f4dfe6
Travis Nickels <nickels.travis@gmail.com>
2025-11-10 12:20:24 -0800

    Add message validation middleware tests

---
+++ src/ClientManager/ClientManager.Shared.Tests/Unit/MessageValidationMiddlewareTests.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ClientManager.Shared.Contracts.Commands;
+using ClientManager.Shared.Messaging;
+using ClientManager.Shared.Tests.Fakes;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+
+namespace ClientManager.Shared.Tests.Unit;
+
+[TestFixture]
+internal class MessageValidationMiddlewareTests
+{
+    CreateClient createClient = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        createClient = new CreateClient
+        {
+            Id = Guid.Empty,
+            FirstName = "Luke",
+            LastName = "Skywalker",
+            Email = "Luke.Skywalker@gmail.com"
+        };
+    }
+
+    [Test]
+    public async Task MessageValidationMiddleware_WhenCreateClientMessageIsVaild_ShouldPass()
+    {
+        var logger = new FakeLogger<MessageValidationMiddleware>();
+
+        var middleware = new MessageValidationMiddleware(logger);
+        var ct = new CancellationToken();
+
+        await middleware.InvokeAsync(createClient, (msg, _) => Task.CompletedTask, ct);
+
+        logger.Logs.Should().Contain(log => log.Level == LogLevel.Debug && log.Message.Contains("passed validation"));
+    }
+
+    [Test]
+    public async Task MessageValidationMiddleware_WhenCreateClientMessageIsMissingFirstName_ShouldWarnWithAnnotationMessage()
+    {
+        var logger = new FakeLogger<MessageValidationMiddleware>();
+
+        var middleware = new MessageValidationMiddleware(logger);
+        var cancellationToken = new CancellationToken();
+
+        createClient.FirstName = string.Empty;
+
+        var ex = await FluentActions
+            .Invoking(() => middleware.InvokeAsync(createClient, (msg, _) => Task.CompletedTask, cancellationToken))
+            .Should()
+            .ThrowAsync<ValidationException>()
+            .Where(ex => ex.Message.Contains("Message validation failed"));
+
+        logger.Logs.Should().Contain(log => log.Level == LogLevel.Warning && log.Message.Contains("Client must have a first name"));
+    }
+}
