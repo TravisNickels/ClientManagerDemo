b1fed9ec812ba476f80edecfc91908ced4521d65
Travis Nickels <nickels.travis@gmail.com>
2025-11-11 11:21:15 -0800

    Add more tests for validation middleware

--- src/ClientManager/ClientManager.Shared.Tests/Unit/MessageValidationMiddlewareTests.cs
+++ src/ClientManager/ClientManager.Shared.Tests/Unit/MessageValidationMiddlewareTests.cs
@@ -12,6 +12,8 @@ internal class MessageValidationMiddlewareTests
 {
     CreateClient createClient = null!;
 
+    record NoAnnotationsMessage : IMessage;
+
     [SetUp]
     public void Setup()
     {
@@ -25,7 +27,7 @@ internal class MessageValidationMiddlewareTests
     }
 
     [Test]
-    public async Task MessageValidationMiddleware_WhenClientIsVaild_ShouldPass()
+    public async Task Should_PassValidation_When_MessageIsValid()
     {
         var logger = new FakeLogger<MessageValidationMiddleware>();
 
@@ -37,22 +39,92 @@ internal class MessageValidationMiddlewareTests
         logger.Logs.Should().Contain(log => log.Level == LogLevel.Debug && log.Message.Contains("passed validation"));
     }
 
-    [Test]
-    public async Task MessageValidationMiddleware_WhenClientIsMissingFirstName_ShouldWarnWithAnnotation()
+    [TestCase("", "Skywalker", "Luke.Skywalker@gmail.com", "first name")]
+    [TestCase("Luke", "", "Luke.Skywalker@gmail.com", "last name")]
+    [TestCase("Luke", "Skywalker", "not-an-email", "valid email")]
+    public async Task Should_LogWarning_When_RequiredFieldIsMissing(string firstName, string lastName, string email, string expectedError)
     {
         var logger = new FakeLogger<MessageValidationMiddleware>();
-
         var middleware = new MessageValidationMiddleware(logger);
         var cancellationToken = new CancellationToken();
 
-        createClient.FirstName = string.Empty;
+        var message = new CreateClient
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email
+        };
 
         var ex = await FluentActions
-            .Invoking(() => middleware.InvokeAsync(createClient, (msg, _) => Task.CompletedTask, cancellationToken))
+            .Invoking(() => middleware.InvokeAsync(message, (_, _) => Task.CompletedTask, cancellationToken))
             .Should()
             .ThrowAsync<ValidationException>()
             .Where(ex => ex.Message.Contains("Message validation failed"));
 
-        logger.Logs.Should().Contain(log => log.Level == LogLevel.Warning && log.Message.Contains("Client must have a first name"));
+        logger.Logs.Should().Contain(log => log.Level == LogLevel.Warning && log.Message.Contains(expectedError, StringComparison.OrdinalIgnoreCase));
+    }
+
+    [Test]
+    public async Task Should_Not_InvokeNext_When_ValidationFails()
+    {
+        var logger = new FakeLogger<MessageValidationMiddleware>();
+        var middleware = new MessageValidationMiddleware(logger);
+        var cancellationToken = new CancellationToken();
+
+        createClient.FirstName = "";
+
+        bool nextCalled = false;
+
+        await FluentActions
+            .Invoking(
+                () =>
+                    middleware.InvokeAsync(
+                        createClient,
+                        (_, _) =>
+                        {
+                            nextCalled = true;
+                            return Task.CompletedTask;
+                        },
+                        cancellationToken
+                    )
+            )
+            .Should()
+            .ThrowAsync<ValidationException>();
+
+        nextCalled.Should().BeFalse("next delegate should not be called when validation fails");
+    }
+
+    [Test]
+    public async Task Should_InvokeNext_When_MessageIsValid()
+    {
+        var logger = new FakeLogger<MessageValidationMiddleware>();
+        var middleware = new MessageValidationMiddleware(logger);
+        var cancellationToken = new CancellationToken();
+
+        bool nextCalled = false;
+
+        await middleware.InvokeAsync(
+            createClient,
+            (msg, _) =>
+            {
+                nextCalled = true;
+                return Task.CompletedTask;
+            },
+            cancellationToken
+        );
+
+        nextCalled.Should().BeTrue();
+    }
+
+    [Test]
+    public async Task Should_PassValidation_When_MessageHasNoValidationAttributes()
+    {
+        var logger = new FakeLogger<MessageValidationMiddleware>();
+        var middleware = new MessageValidationMiddleware(logger);
+        var cancellationToken = new CancellationToken();
+
+        var message = new NoAnnotationsMessage();
+
+        await middleware.Invoking(m => m.InvokeAsync(message, (_, _) => Task.CompletedTask, cancellationToken)).Should().NotThrowAsync();
     }
 }
