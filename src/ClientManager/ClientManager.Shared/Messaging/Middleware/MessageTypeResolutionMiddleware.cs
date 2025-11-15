c434841c36e8240237a15c725d675c69b1fa96ac
Travis Nickels <nickels.travis@gmail.com>
2025-11-15 02:07:55 -0800

    Add message type resolution middleware

---
+++ src/ClientManager/ClientManager.Shared/Messaging/Middleware/MessageTypeResolutionMiddleware.cs
@@ -0,0 +1,16 @@
+﻿namespace ClientManager.Shared.Messaging;
+
+public class MessageTypeResolutionMiddleware(MessageTypeRegistry messageTypeRegistry) : IMessageConsumeMiddleware
+{
+    readonly MessageTypeRegistry _messageTypeRegistry = messageTypeRegistry;
+
+    public async Task InvokeAsync(MessageConsumeContext context, MessageConsumeDelegate next, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(context.Envelope, nameof(context.Envelope));
+
+        var typeName = context.Envelope.MessageType;
+        var resolvedType = _messageTypeRegistry.GetMessageTypeByName(typeName) ?? throw new InvalidOperationException($"No message type found for name: {typeName}");
+        context.MessageType = resolvedType;
+        await next(context, cancellationToken);
+    }
+}
