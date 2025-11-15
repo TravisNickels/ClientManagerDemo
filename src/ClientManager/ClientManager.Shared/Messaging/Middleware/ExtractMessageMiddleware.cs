38a96a4ce9ca74f5df8eefd40332cdcf1d2972f9
Travis Nickels <nickels.travis@gmail.com>
2025-11-15 02:07:19 -0800

    Remove message type resolution from message extraction middleware

--- src/ClientManager/ClientManager.Shared/Messaging/Middleware/ExtractMessageMiddleware.cs
+++ src/ClientManager/ClientManager.Shared/Messaging/Middleware/ExtractMessageMiddleware.cs
@@ -4,13 +4,11 @@ namespace ClientManager.Shared.Messaging;
 
 public class ExtractMessageMiddleware : IMessageConsumeMiddleware
 {
-    readonly IReadOnlyDictionary<string, Type> _messageTypeCache = DiscoverMessageTypes().ToDictionary(t => t.Name, t => t);
-
     public async Task InvokeAsync(MessageConsumeContext context, MessageConsumeDelegate next, CancellationToken cancellationToken = default)
     {
         ArgumentNullException.ThrowIfNull(context, nameof(context));
-        ArgumentNullException.ThrowIfNull(context.MessageType, nameof(context.MessageType));
         ArgumentNullException.ThrowIfNull(context.Envelope, nameof(context.Envelope));
+        ArgumentNullException.ThrowIfNull(context.MessageType, nameof(context.MessageType));
 
         var payload = context.Envelope.Payload;
         context.Message =
@@ -18,11 +16,4 @@ public class ExtractMessageMiddleware : IMessageConsumeMiddleware
 
         await next(context, cancellationToken);
     }
-
-    static IEnumerable<Type> DiscoverMessageTypes() =>
-        AppDomain
-            .CurrentDomain.GetAssemblies()
-            .SelectMany(assembly => assembly.GetTypes())
-            .Where(type => typeof(ICommand).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract)
-            .ToList();
 }
