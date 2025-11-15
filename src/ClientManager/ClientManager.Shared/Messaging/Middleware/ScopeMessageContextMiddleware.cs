c569e220518ddd29ee96fb57a140606280a3c4f8
Travis Nickels <nickels.travis@gmail.com>
2025-11-14 23:47:42 -0800

    Add DI scope for messages

---
+++ src/ClientManager/ClientManager.Shared/Messaging/Middleware/ScopeMessageContextMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ClientManager.Shared.Messaging;
+
+public class ScopeMessageContextMiddleware(IServiceScopeFactory serviceScopeFactory, ILogger<ScopeMessageContextMiddleware> logger) : IMessageConsumeMiddleware
+{
+    readonly IServiceScopeFactory _scopeFactory = serviceScopeFactory;
+    readonly ILogger<ScopeMessageContextMiddleware> _logger = logger;
+
+    public async Task InvokeAsync(MessageConsumeContext context, MessageConsumeDelegate next, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(context.Envelope, nameof(context.Envelope));
+        ArgumentNullException.ThrowIfNull(context.MessageType, nameof(context.MessageType));
+
+        await using var scope = _scopeFactory.CreateAsyncScope();
+
+        var messageContextAccessor = scope.ServiceProvider.GetRequiredService<IMessageContextAccessor>();
+
+        context.MesssageContext = GetMessageContext(context.Envelope);
+        messageContextAccessor.SetCurrentContext(context.MesssageContext);
+
+        context.Scope = scope;
+
+        _logger.LogInformation(
+            "Received message from {messageType}: \n\t[correlationId: {correlationId}]\n\t{@Message}",
+            context.MessageType.Name,
+            context.MesssageContext.CorrelationId,
+            context.Message
+        );
+
+        try
+        {
+            await next(context, cancellationToken);
+        }
+        finally
+        {
+            await scope.DisposeAsync();
+        }
+    }
+
+    static MessageContext GetMessageContext(MessageEnvelope<object> envelope) =>
+        new MessageContext(CorrelationId: envelope.CorrelationId, CausationId: envelope.CausationId, Timestamp: envelope.CreatedUtc);
+}
