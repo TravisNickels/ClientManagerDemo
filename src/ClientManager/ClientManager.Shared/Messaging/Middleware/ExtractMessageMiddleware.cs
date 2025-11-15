8b4919bb874d839614c40f5c4748d1ad35eb1449
Travis Nickels <nickels.travis@gmail.com>
2025-11-14 22:51:04 -0800

    Add extract message middleware

---
+++ src/ClientManager/ClientManager.Shared/Messaging/Middleware/ExtractMessageMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace ClientManager.Shared.Messaging;
+
+public class ExtractMessageMiddleware : IMessageConsumeMiddleware
+{
+    readonly IReadOnlyDictionary<string, Type> _messageTypeCache = DiscoverMessageTypes().ToDictionary(t => t.Name, t => t);
+
+    public async Task InvokeAsync(MessageConsumeContext context, MessageConsumeDelegate next, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(context, nameof(context));
+        ArgumentNullException.ThrowIfNull(context.MessageType, nameof(context.MessageType));
+        ArgumentNullException.ThrowIfNull(context.Envelope, nameof(context.Envelope));
+
+        var messageJson = (JsonElement)context.Envelope.Message;
+        context.Message =
+            messageJson.Deserialize(context.MessageType) ?? throw new InvalidOperationException($"Failed to deserialize message of type {context.MessageType.FullName}.");
+
+        await next(context, cancellationToken);
+    }
+
+    static IEnumerable<Type> DiscoverMessageTypes() =>
+        AppDomain
+            .CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => typeof(ICommand).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract)
+            .ToList();
+}
