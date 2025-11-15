eff5017ab0fc07f9af2219f095ab115d224187ac
Travis Nickels <nickels.travis@gmail.com>
2025-11-14 22:50:34 -0800

    Add extract envelope middleware

---
+++ src/ClientManager/ClientManager.Shared/Messaging/Middleware/ExtractEnvelopeMiddleware.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace ClientManager.Shared.Messaging.Middleware;
+
+public class ExtractEnvelopeMiddleware : IMessageConsumeMiddleware
+{
+    public async Task InvokeAsync(MessageConsumeContext context, MessageConsumeDelegate next, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(context, nameof(context));
+
+        var deserializedEnvelope =
+            JsonSerializer.Deserialize<MessageEnvelope<object>>(context.RawJson) ?? throw new InvalidOperationException("Failed to deserialize message envelope.");
+
+        context.Envelope = deserializedEnvelope;
+
+        await next(context, cancellationToken);
+    }
+}
