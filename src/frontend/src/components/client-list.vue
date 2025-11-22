b09664e51543db0593886e13a9736a688905819a
Travis Nickels <nickels.travis@gmail.com>
2025-11-22 03:07:03 -0800

    Add client list component

---
+++ src/frontend/src/components/client-list.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Client } from '@/types/client'
+
+const props = defineProps<{
+  clients: Client[]
+  showArchivedClients: boolean
+}>()
+</script>
+
+<template>
+  <ul class="list-group">
+    <li v-for="client in props.clients" :key="client.id" class="list-group-item d-flex justify-content-between align-items-center">
+      {{ client.firstName }} {{ client.lastName }}
+      <span v-if="!client.isArchived" class="badge bg-primary rounded-pill">Active</span>
+      <span v-if="client.isArchived" class="badge bg-secondary rounded-pill">Archived</span>
+    </li>
+  </ul>
+</template>
