664987bbc2a0f35c35f6f25f69ff3e5e73541276
Travis Nickels <nickels.travis@gmail.com>
2025-11-23 01:39:11 -0800

    Add update client contracts

---
+++ src/ClientManager/ClientManager.Shared/Contracts/Commands/UpdateClient.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ClientManager.Shared.Messaging;
+
+namespace ClientManager.Shared.Contracts.Commands;
+
+public class UpdateClient : ICommand
+{
+    [Required(ErrorMessage = "Client ID required to update client.")]
+    public Guid Id { get; set; }
+
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+
+    [EmailAddress(ErrorMessage = "Client must have a valid email address.")]
+    public string Email { get; set; } = default!;
+
+    public bool IsArchived { get; set; } = default!;
+}
---
+++ src/ClientManager/ClientManager.Shared/Contracts/Events/ClientUpdated.cs
@@ -0,0 +1,15 @@
+﻿using ClientManager.Shared.DTOs.Responses;
+using ClientManager.Shared.Messaging;
+
+namespace ClientManager.Shared.Contracts.Events;
+
+public class ClientUpdated : IEvent, IEventToResponse<ClientResponse>
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public bool IsArchived { get; set; } = default!;
+
+    public ClientResponse ToResponse() => new(Id, FirstName, LastName, Email, IsArchived);
+}
