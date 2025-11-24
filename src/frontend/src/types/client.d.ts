f900085f41517ff7b337d6a00e025ca90f7617ca
Travis Nickels <nickels.travis@gmail.com>
2025-11-24 14:05:31 -0800

    Update client type in fronetend

--- src/frontend/src/types/client.d.ts
+++ src/frontend/src/types/client.d.ts
@@ -1,11 +1,21 @@
+import type { CreatePhoneRequest, Phone } from './phone'
+
 export interface Client {
   id: string
   firstName: string
   lastName: string
   email: string
   isArchived: boolean
+  phones?: Phone[]
 }
 
-export type CreateClientRequest = Omit<Client, 'id' | 'isArchived'>
+export interface CreateClientRequest {
+  firstName: string
+  lastName: string
+  email: string
+  isArchived: boolean
+  phones?: CreatePhoneRequest[]
+}
+
 export type UpdateClientRequest = Client
 export type ClientResponse = Client
