0653d83c2bb7683fb76ea783524ec734e051d94f
Travis Nickels <nickels.travis@gmail.com>
2025-11-24 17:48:25 -0800

    Add Delete client handler

---
+++ src/ClientManager/ClientManager.Worker/Handlers/Client/DeleteClientHandler.cs
@@ -0,0 +1,24 @@
+﻿using ClientManager.Shared.Contracts.Commands;
+using ClientManager.Shared.Contracts.Events;
+using ClientManager.Shared.Messaging;
+using ClientManager.Worker.Repositories;
+
+namespace ClientManager.Worker.Handlers;
+
+public class DeleteClientHandler(IClientRepository clientRepository, IMessagePublisher messagePublisher, ILogger<CreateClientHandler> logger) : IHandleMessage<DeleteClient>
+{
+    readonly IClientRepository _clientRepository = clientRepository;
+    readonly ILogger<CreateClientHandler> _logger = logger;
+    readonly IMessagePublisher _messagePublisher = messagePublisher;
+
+    public async Task HandleAsync(DeleteClient message, MessageContext context, CancellationToken cancellationToken)
+    {
+        await _clientRepository.DeleteAsync(message.Id, cancellationToken);
+
+        _logger.LogInformation("Client {id} deleted", message.Id);
+
+        var deletedEvent = new ClientDeleted { Id = message.Id };
+
+        await _messagePublisher.PublishAsync(deletedEvent, cancellationToken);
+    }
+}
