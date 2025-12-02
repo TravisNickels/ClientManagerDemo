cba3f96cc7beabd985fef21c30a1435fd9f086f4
Travis Nickels <nickels.travis@gmail.com>
2025-12-01 23:16:30 -0800

    Update seen data to match mask

--- src/ClientManager/ClientManager.Shared/Data/AppDbContext.cs
+++ src/ClientManager/ClientManager.Shared/Data/AppDbContext.cs
@@ -58,28 +58,28 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
                 {
                     Id = Guid.Parse("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"),
                     ClientId = Guid.Parse("11111111-1111-1111-1111-111111111111"),
-                    Number = "111-111-1111",
+                    Number = "+1 (111) 111-1111",
                     Type = "Home"
                 },
                 new Phone
                 {
                     Id = Guid.Parse("bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb"),
                     ClientId = Guid.Parse("22222222-2222-2222-2222-222222222222"),
-                    Number = "222-222-2222",
+                    Number = "+1 (222) 222-2222",
                     Type = "Home"
                 },
                 new Phone
                 {
                     Id = Guid.Parse("cccccccc-cccc-cccc-cccc-cccccccccccc"),
                     ClientId = Guid.Parse("33333333-3333-3333-3333-333333333333"),
-                    Number = "333-333-3333",
+                    Number = "+1 (333) 333-3333",
                     Type = "Home"
                 },
                 new Phone
                 {
                     Id = Guid.Parse("dddddddd-dddd-dddd-dddd-dddddddddddd"),
                     ClientId = Guid.Parse("44444444-4444-4444-4444-444444444444"),
-                    Number = "444-444-4444",
+                    Number = "+1 (444) 444-4444",
                     Type = "Home"
                 }
             );
