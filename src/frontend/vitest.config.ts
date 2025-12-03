e4df27f4fc9c874aee86fc4b434191c2ef69d5b5
Travis Nickels <nickels.travis@gmail.com>
2025-12-03 00:49:13 -0800

    Add test setup

--- src/frontend/vitest.config.ts
+++ src/frontend/vitest.config.ts
@@ -7,8 +7,9 @@ export default mergeConfig(
   defineConfig({
     test: {
       environment: 'jsdom',
+      setupFiles: ['tests/setup.ts'],
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
-  }),
+  })
 )
