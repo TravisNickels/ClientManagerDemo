2e8c5a188c47527472a46354f49a399902ef3b2e
Travis Nickels <nickels.travis@gmail.com>
2025-12-03 01:04:34 -0800

    Fix config to remove error about expect is not defined

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -16,4 +16,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
+    include: ['tests/**/*.spec.ts'],
+  },
 })
