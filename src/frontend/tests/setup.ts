07daa6c23cf0f04d002f64d20b95055eb129f408
Travis Nickels <nickels.travis@gmail.com>
2025-12-04 12:44:14 -0800

    Remove pinia creation from setup because render.ts handles that now

--- src/frontend/tests/setup.ts
+++ src/frontend/tests/setup.ts
@@ -1,7 +1,3 @@
-import { beforeEach } from 'vitest'
-import { createPinia, setActivePinia } from 'pinia'
 import '@testing-library/jest-dom'
 
-beforeEach(() => {
-  setActivePinia(createPinia())
 })
