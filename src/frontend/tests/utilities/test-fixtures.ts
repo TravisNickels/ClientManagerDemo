ed077aa1862386a4b82c0aee8179c047c4aa62e3
Travis Nickels <nickels.travis@gmail.com>
2025-12-05 01:18:40 -0800

    Create a test fixture utility for test setups

---
+++ src/frontend/tests/utilities/test-fixtures.ts
@@ -0,0 +1,17 @@
+import type { Client } from '@/types/client'
+import { renderWithApp } from './render'
+import Dashboard from '@/pages/client-dashboard.vue'
+import { useClientStore } from '@/stores/clientStore'
+
+export async function setupDashboard(clients: Client[]) {
+  const wrapper = await renderWithApp(Dashboard, {
+    useRealSignalR: false,
+    useRealPinia: false,
+  })
+
+  const store = useClientStore()
+  store.isLoading = false
+  store.allClients = clients
+
+  return { wrapper, store }
+}
