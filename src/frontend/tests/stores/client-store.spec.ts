9dbdad4ed3a62f21669b680d71afcdea333426ca
Travis Nickels <nickels.travis@gmail.com>
2025-12-06 00:01:46 -0800

    Add test for client store

---
+++ src/frontend/tests/stores/client-store.spec.ts
@@ -0,0 +1,34 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { mockAxiosGet } from '@tests/utilities/axios-mock'
+import { setActivePinia, createPinia } from 'pinia'
+import { useClientStore } from '@/stores/client-store'
+
+describe('FEATURE: Client store', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+    mockAxiosGet.mockReset()
+  })
+  describe('RULE: Client list should be kept in state', () => {
+    it('EXAMPLE: Fetched clients are stored in allClients', async () => {
+      // Arrange
+      const store = useClientStore()
+      const clients = [
+        {
+          id: '11111111-1111-1111-1111-111111111111',
+          firstName: 'Luke',
+          lastName: 'Skywalker',
+          email: 'Luke.Skywalker@gmail.com',
+          isArchived: false,
+        },
+      ]
+      mockAxiosGet.mockResolvedValue({ data: clients })
+
+      // Act
+      await store.updateClientsList()
+
+      // Arrange
+      expect(store.allClients.length).toBe(1)
+      expect(store.allClients[0]!.firstName).toBe('Luke')
+    })
+  })
+})
