0bc2a35e38c88cc66439f1829d4e8eafa1dddca3
Travis Nickels <nickels.travis@gmail.com>
2025-12-08 17:42:01 -0800

    Update test setup to clear mocks and remove DOM dump

--- src/frontend/tests/setup.ts
+++ src/frontend/tests/setup.ts
@@ -1,6 +1,23 @@
-import { afterEach } from 'vitest'
+import '@tests/utilities/mocks/signalr-mock'
+import '@tests/utilities/mocks/axios-mock'
 import '@testing-library/jest-dom'
+
+import { afterEach, beforeEach, vi } from 'vitest'
+import { configure } from '@testing-library/vue'
+import { mockAxiosGet } from '@tests/utilities/mocks/axios-mock'
+
+beforeEach(() => {
+  mockAxiosGet.mockReset()
+  vi.clearAllMocks()
+})
 
 afterEach(() => {
   localStorage.clear()
+})
+
+// Remove DOM dump from errors
+configure({
+  getElementError(message) {
+    return new Error(message ?? undefined) // do NOT append PrettyDOM
+  },
 })
