dde3011ab449ec33bbc6b56432aba9778584c2b0
Travis Nickels <nickels.travis@gmail.com>
2025-12-08 17:44:51 -0800

    Update render and test fixtures

--- src/frontend/tests/utilities/render.ts
+++ src/frontend/tests/utilities/render.ts
@@ -4,9 +4,10 @@ import { createMemoryHistory, createRouter, type Router, type RouteRecordRaw } f
 import { type Component } from 'vue'
 import { vi } from 'vitest'
 import { createPinia, type Pinia } from 'pinia'
-import { signalrConfig } from './signalr-mock'
 import ClientDashboard from '@/pages/client-dashboard.vue'
 import ClientManagement from '@/pages/client-management.vue'
+import { useSignalRStore, type SignalRStoreInstance } from '@/stores/signalr-store'
+import { useClientStore } from '@/stores/client-store'
 
 export interface RenderWithAppOptions<T extends Component = Component> extends RenderOptions<T> {
   useRealPinia?: boolean
@@ -16,31 +17,21 @@ export interface RenderWithAppOptions<T extends Component = Component> extends R
 }
 
 export async function renderWithApp<T extends Component>(component: T, options: RenderWithAppOptions<T> = {}) {
-  const pinia: TestingPinia | Pinia = options.useRealPinia ? createPinia() : createTestingPinia({ createSpy: (fn) => vi.fn(fn), stubActions: true })
+  const pinia: Pinia | TestingPinia = getPinia(options.useRealPinia)
   const router: Router = createRouter({
     history: createMemoryHistory(),
     routes: options.routes ?? [
-      {
-        path: '/',
-        redirect: '/client-dashboard',
-      },
-      {
-        path: '/client-dashboard',
-        name: 'client-dashboard',
-        component: ClientDashboard,
-      },
-      {
-        path: '/client/:id',
-        name: 'client-management',
-        component: ClientManagement,
-      },
+      { path: '/', redirect: '/client-dashboard' },
+      { path: '/client-dashboard', name: 'client-dashboard', component: ClientDashboard },
+      { path: '/client/:id', name: 'client-management', component: ClientManagement },
     ],
   })
 
-  await router.push(options.initialRoute ?? '/client-dashboard')
-  await router.isReady()
+  const signalRStore = useSignalRStore(pinia) as SignalRStoreInstance
+  const clientStore = useClientStore(pinia)
 
-  signalrConfig.useRealSignalR = !!options.useRealSignalR
+  await router.push(options.initialRoute ?? '/')
+  await router.isReady()
 
   const renderedComponent = render(component, {
     ...options,
@@ -50,5 +41,9 @@ export async function renderWithApp<T extends Component>(component: T, options: 
     },
   })
 
-  return { renderedComponent, router }
+  return { renderedComponent, router, pinia, signalRStore, clientStore }
+}
+
+function getPinia(useRealPinia: boolean = false, stub: boolean = false): TestingPinia | Pinia {
+  return useRealPinia ? createPinia() : createTestingPinia({ createSpy: (fn) => vi.fn(fn), stubActions: stub })
 }
--- src/frontend/tests/utilities/test-fixtures.ts
+++ src/frontend/tests/utilities/test-fixtures.ts
@@ -3,46 +3,51 @@ import { renderWithApp } from './render'
 import Dashboard from '@/pages/client-dashboard.vue'
 import DashboardHeader from '@/components/dashboard-header.vue'
 import ClientForm from '@/components/client-form.vue'
-import { useClientStore } from '@/stores/client-store'
 import App from '@/App.vue'
 
-const defaultClients: Client[] = [
+export const defaultClients: Client[] = [
   { id: '11111111-1111-1111-1111-111111111111', firstName: 'Luke', lastName: 'Skywalker', email: 'Luke.Skywalker@gmail.com', isArchived: false },
   { id: '22222222-2222-2222-2222-222222222222', firstName: 'Han', lastName: 'Solo', email: 'han.solo@gmail.com', isArchived: false },
   { id: '33333333-3333-3333-3333-333333333333', firstName: 'Darth', lastName: 'Vader', email: 'darth.vader@gmail.com', isArchived: true },
   { id: '44444444-4444-4444-4444-444444444444', firstName: 'Lord', lastName: 'Sidious', email: 'lord.sidious@gmail.com', isArchived: true },
 ]
 
-export async function setupDefaultApp(initialRoute: string) {
-  const { renderedComponent, router } = await renderWithApp(App, {
-    useRealSignalR: false,
+export async function setupApp(initialRoute: string = '/') {
+  const { renderedComponent, router, signalRStore, clientStore } = await renderWithApp(App, {
     useRealPinia: false,
     initialRoute: initialRoute,
   })
 
-  const store = useClientStore()
-  store.isLoading = false
-  store.allClients = defaultClients
+  clientStore.isLoading = false
+  clientStore.allClients = defaultClients
 
-  return { renderedComponent, router, store }
+  return { renderedComponent, router, clientStore, signalRStore }
 }
 
-export async function setupDashboard(clients: Client[] = defaultClients) {
-  const { renderedComponent, router } = await renderWithApp(Dashboard, {
-    useRealSignalR: false,
+export async function setupDefaultApp(initialRoute: string = '/client-dashboard') {
+  const { renderedComponent, router, clientStore } = await renderWithApp(App, {
     useRealPinia: false,
+    initialRoute: initialRoute,
+  })
+  clientStore.isLoading = false
+  clientStore.allClients = defaultClients
+
+  return { renderedComponent, router, clientStore }
+}
+
+export async function setupDashboard(clients: Client[] = defaultClients, useRealPinia: boolean = false) {
+  const { renderedComponent, router, pinia, signalRStore, clientStore } = await renderWithApp(Dashboard, {
+    useRealPinia: useRealPinia,
   })
 
-  const store = useClientStore()
-  store.isLoading = false
-  store.allClients = clients
+  clientStore.isLoading = false
+  clientStore.allClients = clients
 
-  return { renderedComponent, router, store }
+  return { renderedComponent, router, clientStore, signalRStore, pinia }
 }
 
 export async function setupDashboardHeader(clients: Client[] = defaultClients) {
   return await renderWithApp(DashboardHeader, {
-    useRealSignalR: true,
     useRealPinia: false,
     props: {
       clients: clients,
