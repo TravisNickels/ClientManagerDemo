e0b68b00d323947111582010f8c25ad3fcad71c4
Travis Nickels <nickels.travis@gmail.com>
2025-12-08 17:47:30 -0800

    Add more signalr types for mapping

--- src/frontend/src/types/signalr/signalrEvents.ts
+++ src/frontend/src/types/signalr/signalrEvents.ts
@@ -8,7 +8,9 @@ export type SignalrEvents = {
   [K in keyof typeof eventDefinitions]: (typeof eventDefinitions)[K]
 }
 
-export type SignalrEventName = keyof typeof eventDefinitions
+export type SignalrEventMap = typeof eventDefinitions
+export type SignalrEventName = keyof SignalrEventMap
+export type SignalREventHandler<K extends SignalrEventName = SignalrEventName> = (payload: SignalrEventMap[K]) => void
 
 export const SignalrEventNames: Record<SignalrEventName, SignalrEventName> = Object.fromEntries(
   Object.keys(eventDefinitions).map((k) => [k, k])
